550aa0c4f7087f1725a40c1e8bcd6de71b98e11a
aleksandr-belyaev <belyaev.photo@gmail.com>
2021-01-25 22:15:24 +0700

    Image stub, buttons and outlets for all of them

--- Photoeditor/ViewController.swift
+++ Photoeditor/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sourceImage: UIImageView!
+    @IBOutlet weak var rotateButton: UIButton!
+    @IBOutlet weak var invertColorsButton: UIButton!
+    @IBOutlet weak var mirrorImageButton: UIButton!
+    
+    @IBAction func doRotate(_ sender: Any) {
+        sourceImage.transform = sourceImage.transform.rotated(by: .pi)
+    }
+    
+    @IBAction func doInvertColors(_ sender: Any) {
+    }
+    
+    @IBAction func doMirrorImage(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
